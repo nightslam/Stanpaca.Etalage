af01741760d02fe42cca823520ed4215afde0adb
rickz <rickz@PC-RICK>
2025-05-23 21:31:39 +0200

    Echt producten toegevoegd

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,7 +5,7 @@ public class Product
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public bool IsSold { get; set; }
-    public bool IsHighlight { get; set; }
+    public decimal Price { get; set; } = 0;
+    public bool IsAvailable { get; set; } = true;
+    public string SpecialEvent { get; set; } = string.Empty;
 } 
